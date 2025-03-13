6142f4193eca98ded11969ac37f07c130d3e233a
surajkr20 <suraj71308kumar@gmail.com>
2025-03-13 08:03:23 +0530

    getting meeting id

--- src/app/video-meeting/[roomId]/page.jsx
+++ src/app/video-meeting/[roomId]/page.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+"use client"
+
+import { useSession } from 'next-auth/react';
+import { useParams, useRouter } from 'next/navigation';
+import React from 'react';
 
 const page = () => {
+  const params = useParams();
+  const roomId = params.roomId;
+  const {data: session, status} = useSession();
+  const router = useRouter();
   return (
     <div>page</div>
   )
